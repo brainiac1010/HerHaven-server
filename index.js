efc33c0b34d6ea82f2d0b258604c3b653f888518
Arman Hossain <armanmahim00@gmail.com>
2025-04-07 15:47:09 +0600

    updatedd

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: 'http://localhost:5173', 
+    origin: 'https://her-haven-xi.vercel.app', 
     credentials: true,
   })
 );
